ff4b6b6e256cb9a9ad49bac684d54e5c4de33ff2
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-06-13 09:39:50 -0700

    Progress commit. Displaying friend data to broswer. About to try implementing dynamic routing to each piece of friend data

---
+++ friends/src/components/FriendCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom';
+import './FriendCard.scss';
+
+export default function FriendCard(props) {
+	const {age, email, id, name} = props.friend
+	return (
+		<div>
+			<div className="friend">
+				<h2>{name}</h2>
+				<p>{email}</p>
+				<p>Age: {age}</p>
+			</div>
+		</div>
+	)
+}
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+import FriendCard from './FriendCard';
+
+export default class FriendList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			friends: []
+		}
+	}
+
+	componentDidMount() {
+		axios
+			.get('http://localhost:5000/friends')
+			.then(response => this.setState( {friends: response.data}))
+			.catch(error => console.log(error));
+	}
+
+	render() {
+		return (
+			<div className="friend-list">
+				{this.state.friends.map(friend => {
+					return <FriendCard key={friend.id} friend={friend} />
+				})}
+			</div>
+		)
+	}
+}
